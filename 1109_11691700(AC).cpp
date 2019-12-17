63e20f4aa5c801fbeda5a7fd92821fa24e67b391
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-17 15:04:21 +0900

    correct1109
    코드업1109에 대한 문제풀이

---
+++ 1109_11691700(AC).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int iAge;
+	float fPrivCode;
+	string sName;
+	char cDeptCode;
+
+	cin >> sName >> iAge >> cDeptCode >> fPrivCode;
+	cout << sName << endl;
+	cout << iAge << endl;
+	cout << cDeptCode << endl;
+	cout << fPrivCode << endl;
+}
+
