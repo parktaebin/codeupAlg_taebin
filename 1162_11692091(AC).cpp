8e2c5ef0b77f2b01511b55d14feba3f08d079f1c
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-17 15:42:03 +0900

    correct 1162
    1162번 문제풀이

---
+++ 1162_11692091(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int iYear, iMonth, iDay,iJum;
+	cin >> iYear >> iMonth >> iDay;
+
+	iJum = (iYear - iMonth + iDay)%10;
+	if (iJum == 0) {
+		cout << "대박" << endl;
+	}
+	else {
+		cout << "그럭저럭" << endl;
+	}
+}
+
