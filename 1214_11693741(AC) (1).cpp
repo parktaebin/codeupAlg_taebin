b01595ac76d0376de8585fb4624823a846ca20c8
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-17 18:21:11 +0900

    correct 1214
    코드업 1214

---
+++ 1214_11693741(AC) (1).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{	
+	int iYear, iMonth, iDay;
+
+	cin >> iYear >> iMonth;
+
+	if (iMonth / 2 == 1) {
+		if (iYear % 400 == 0 ||(iYear % 4 == 0 && iYear % 100 != 0)) {
+			iDay = 29;
+		}
+		else {
+			iDay = 28;
+		}
+	}
+	else {
+			if (iMonth <= 7) {
+				if (iMonth % 2 == 1) {
+					iDay = 31;
+				}
+				else {
+					iDay = 30;
+				}
+			}
+			else {
+				if (iMonth % 2 == 0) {
+					iDay = 31;
+				}
+				else {
+					iDay = 30;
+				}
+			}
+		}
+	cout << iDay << endl;
+	return 0;
+}
