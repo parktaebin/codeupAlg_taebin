e1f718bc968fbc9782fc70ab623067e522b84a1c
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-17 15:32:34 +0900

    correct 1127
    코드업 1127문제풀이

---
+++ 1127_11692025(AC).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int iMidScore, iFinScore, iPerformScore;
+	float fMidProportion, fFinProportion, fPerformProPortion, fTotalScore;
+	cin >> fMidProportion >> iMidScore;
+	cin >> fFinProportion >> iFinScore;
+	cin >> fPerformProPortion >> iPerformScore;
+
+	fTotalScore = (fMidProportion*iMidScore)+(fFinProportion*iFinScore)+(fPerformProPortion * iPerformScore);
+	printf("%.1f", fTotalScore);
+}
