e68fd82c890ee3c497c8fb5931deb9f118e2b848
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-17 15:48:26 +0900

    correct 1230
    코드업 1230풀이과정

---
+++ 1230_11692149(AC).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int iFirTunnelHeight, iSecTunnelHeight, iThirTunnelHeight;
+	cin >> iFirTunnelHeight >> iSecTunnelHeight >> iThirTunnelHeight;
+
+	if (iFirTunnelHeight <= 170) {
+		cout << "CRASH" << " " << iFirTunnelHeight << endl;
+	}
+	else if (iSecTunnelHeight <= 170) {
+		cout << "CRASH" << " " << iSecTunnelHeight << endl;
+	}
+	else if (iThirTunnelHeight <= 170) {
+		cout << "CRASH" << " " << iThirTunnelHeight << endl;
+	}
+	else {
+		cout << "PASS" << endl;
+	}
+	return 0;
+}
+
