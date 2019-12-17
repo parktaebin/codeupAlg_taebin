abce93bb6a308e95576ff28a911b580daaed2991
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-17 15:13:53 +0900

    correct1126
    코드업 1126에 관한 풀이

---
+++ 1126_11691804(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int iFirstNum, iSecondNum;
+	cin >> iFirstNum >> iSecondNum;
+
+	cout << iFirstNum <<" + "<< iSecondNum <<" = "<< iFirstNum + iSecondNum << endl;
+	cout << iFirstNum <<" - "<< iSecondNum <<" = "<< iFirstNum - iSecondNum << endl;
+	cout << iFirstNum <<" * "<< iSecondNum <<" = "<< iFirstNum * iSecondNum << endl;
+	cout << iFirstNum <<" / "<< iSecondNum <<" = "<< iFirstNum / iSecondNum << endl;
+	cout << iFirstNum <<" % "<< iSecondNum <<" = "<< iFirstNum % iSecondNum << endl;
+
+}
+
