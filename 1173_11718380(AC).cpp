03486e30df1de8c6949dd6e6a2f3bf2f82d3959b
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 08:54:54 +0900

    correct 1173
    나머지를이용해서 시간 바꿔주기(반복되는 구간이용)

---
+++ 1173_11718380(AC).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{	
+	int iHour, iMinutes;
+	cin >> iHour >> iMinutes;
+	if (iMinutes >= 30) {
+		iMinutes = iMinutes - 30;
+	}
+	else {
+		iHour=(iHour + 23) % 24;
+		iMinutes = (iMinutes + 30) % 60;
+	}
+
+	cout << iHour <<" "<<iMinutes << endl;
+
+	return 0;
+}
