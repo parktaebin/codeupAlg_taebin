e478f11e3d376f9e798e7e3208c05f273183aac5
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 13:18:12 +0900

    correct 1675
    아스키코드를 이용해서 조건분기 및 c++ string 내장함수 at(),length()함수 이용

---
+++ 1675_11720222(AC).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string sMessage;
+	char sPrintVolcab, beforeChangeVolcab;
+	int iIndex;
+	getline(cin, sMessage, '\n');
+
+	for (iIndex = 0; iIndex < sMessage.length(); iIndex++) {
+
+		beforeChangeVolcab = sMessage.at(iIndex);
+
+		if (beforeChangeVolcab == ' ') {
+			sPrintVolcab = ' ';
+		}
+		else if (beforeChangeVolcab <= 'c') {
+			sPrintVolcab = beforeChangeVolcab +23;
+		}
+		else {
+			sPrintVolcab = beforeChangeVolcab - 3;
+		}
+		cout << sPrintVolcab;
+	}
+	return 0;
+}
