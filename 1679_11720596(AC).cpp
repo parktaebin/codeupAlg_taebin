95782980ee69adcce4a9341f2cf00d6fb4ea9bb5
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 14:16:35 +0900

    correct 1679
    3중중첩반복문을 통해 조건일치 하는 것만 출력하기

---
+++ 1679_11720596(AC).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int a, b, c, iLength,iCount;
+	cin >> iLength;
+	iCount = 0;
+	for (a = 1; a <= iLength / 2; a++) {
+		for (b = a; b <= iLength / 2; b++) {
+			for (c = b; c <= iLength / 2; c++) {
+
+				if (a + b + c == iLength && a + b > c) {
+					iCount++;
+					cout << a << " " << b << " " << c<<" "<< endl;
+				}
+			}
+		}
+	}
+	if (iCount == 0) {
+		cout << -1;
+	}
+	return 0;
+}
