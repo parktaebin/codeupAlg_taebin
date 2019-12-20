5f679ffad62816497f5fb3ceb8f68b9661d09c48
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 09:21:40 +0900

    correct 1290
    약수구하기, 반복횟수 줄이기 위해 입력한수의/2가 될때까지만 반복

---
+++ 1290_11718628(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int iIndex, iLength,iNumber;
+	cin >> iLength;
+	iNumber = 0;
+	for (iIndex = 1; iIndex <= iLength / 2; iIndex++) {
+		if (iLength % iIndex == 0) {
+			iNumber++;
+		}
+	}
+	cout << iNumber;
+	return 0;
+}
