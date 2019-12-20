f28e656cf006b185601a84a3c3398b81f7894f32
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 09:03:33 +0900

    correct 1253
    임시변수를 이용해서 숫자바꾸기, 반복출력

---
+++ 1253_11718484(AC).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{	
+	int a, b,iTemp,iIndex;
+	cin >> a >> b;
+	if (a > b) {
+		iTemp = a;
+		a = b;
+		b = iTemp;
+	}
+	for (iIndex = a; iIndex < b + 1; iIndex++) {
+		cout << iIndex << " ";
+	}
+	return 0;
+}
+
