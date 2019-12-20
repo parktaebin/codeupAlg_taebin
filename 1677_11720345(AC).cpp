197c342240f56f13b7b0602720b067b26a67e480
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 13:50:01 +0900

    correct 1677
    반복문 3개(첫번째행,마지막행,열)
    열표현은 이중반복문사용

---
+++ 1677_11720345(AC).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int iIndex, iWidth, iHeight,iColumn;
+	cin >> iWidth >> iHeight;
+	
+	cout << "+";
+	for (iIndex = 0; iIndex < iWidth - 2; iIndex++) {
+		cout << "-";
+	}
+	cout << "+"<<endl;
+	for (iIndex = 0; iIndex < iHeight - 2; iIndex++) {
+		cout << "|";
+		for (iColumn = 0; iColumn < iWidth - 2; iColumn++) {
+			cout << " ";
+		}
+		cout << "|"<<endl;
+	}
+	cout << "+";
+	for (iIndex = 0; iIndex < iWidth - 2; iIndex++) {
+		cout << "-";
+	}
+	cout << "+" << endl;
+
+	return 0;
+}
