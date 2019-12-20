f2bb17f189343f904065950ed4862991e7148ad8
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 15:14:46 +0900

    correct1292

---
+++ 1292_11721113(AC).cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <string>
+
+	using namespace std;
+	int main()
+	{
+		long iDna, iSum, iLength;
+		string sDna;
+		int iTemp;
+
+		iSum = 0;
+		cin >> sDna;
+		iLength = sDna.length();
+		for (int iIndex = 0; iIndex < iLength; ++iIndex) {
+			iTemp = sDna.at(iIndex) - '0';
+			iSum = iSum + iTemp;
+		}
+
+		if (iSum % 7 == 4) {
+			cout << "suspect" << endl;
+		}
+		else {
+			cout << "citizen" << endl;
+		}
+		return 0;
+	}
+
+
