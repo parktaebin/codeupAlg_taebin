8a90f1f37a65c1a613a1e33c6c85d39f8b31390f
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 13:33:16 +0900

    correct 1293
    메모리 동적할당이용, 첫번째 number cin 받아서 배열생성

---
+++ 1293_11720295(AC).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int number, iIndex, iMax,iMin;
+	cin >> number;
+	int *pScore = new int[number];					//학생의 수를 나중에 입력받기때문에 동적할당
+	
+	for (iIndex = 0; iIndex < number; iIndex++) {
+		cin >> pScore[iIndex];						//포인터를 배열처럼 사용		
+	}
+	iMax= pScore[0];
+	iMin= pScore[0];
+	for (iIndex = 1; iIndex < number; iIndex++) {
+		if (pScore[iIndex] > iMax) {
+			iMax = pScore[iIndex];
+		}
+		if (pScore[iIndex] < iMin) {
+			iMin = pScore[iIndex];
+		}
+	}
+	cout << iMax << " " << iMin << endl;
+	return 0;
+}
