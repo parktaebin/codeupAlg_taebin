1b8d00e8ab8338cc6dcbfb059deb69c64e187353
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-20 09:14:49 +0900

    correct 1289
    코드재사용성을 위해 배열크기 지정,
    이후 비교를 통해 최댓값 구하기

---
+++ 1289_11718596(AC).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<string>
+#define ARRAY_SIZE 3
+using namespace std;
+
+int main()
+{	
+	int iWidth[ARRAY_SIZE],iHeight[ARRAY_SIZE],iSize[ARRAY_SIZE],iIndex, iMaxSize;
+
+	for (iIndex = 0; iIndex < ARRAY_SIZE; iIndex++) {
+		cin >> iWidth[iIndex] >> iHeight[iIndex];
+		iSize[iIndex] = iWidth[iIndex] * iHeight[iIndex];
+	}
+	iMaxSize = iSize[0];
+	for (iIndex = 1; iIndex < ARRAY_SIZE; iIndex++) {
+		if (iSize[iIndex] > iMaxSize) {
+			iMaxSize = iSize[iIndex];
+		}
+	}
+	cout << iMaxSize;
+	return 0;
+}
