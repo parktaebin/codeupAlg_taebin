3f08413f9d159ceb2f955a9776fb1c4ce4f8847f
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-22 10:10:15 +0900

    correct 1413
    문자 거꾸로 출력하기

---
+++ 1413_11734467(AC).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+	using namespace std;
+	int main()
+	{
+		int iIndex;
+		string sMessage;
+		getline(cin, sMessage, '\n');
+		string* pstr = new string[sMessage.length()];
+		for (iIndex = 0; iIndex < sMessage.length(); iIndex++) {
+			pstr[sMessage.length() - (iIndex + 1)] = sMessage.at(iIndex);
+		}
+		for (iIndex = 0; iIndex < sMessage.length(); iIndex++) {
+			cout << pstr[iIndex];
+		}
+		return 0;
+	}
