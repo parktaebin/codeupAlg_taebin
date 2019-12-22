5189ca1844ebfaabbe20715e0d4ff6144a14f2c7
parktaebin <58967138+parktaebin@users.noreply.github.com>
2019-12-22 10:00:52 +0900

    correct 1408

---
+++ 1408_11734432(AC).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+	using namespace std;
+	int main()
+	{
+		int iIndex;
+		string sMessage;
+		cin >> sMessage;
+		string* pStr = new string [sMessage.length()];
+		string* pStr2 = new string[sMessage.length()];
+		for (iIndex = 0; iIndex < sMessage.length(); iIndex++) {
+			pStr[iIndex]=sMessage.at(iIndex)+2;
+			cout << pStr[iIndex];
+		}
+		cout << endl;
+		for (iIndex = 0; iIndex < sMessage.length(); iIndex++) {
+			pStr2[iIndex]=(sMessage.at(iIndex)*7)%80+48 ;
+			cout << pStr2[iIndex];
+		}
+		return 0;
+	}
