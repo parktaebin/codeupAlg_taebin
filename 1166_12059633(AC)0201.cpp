47a7c5a3177413dd07cef7ed79d01dd1b871bdae
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-02 22:48:26 +0900

    Create 1166_12059633(AC)0201.cpp
    2월이 29일까지 있는 해를 윤년이라고 한다.
    
    어떤 해가 입력되면 그 해가 윤년인지 아닌지 판별하시오.
    
    윤년 판단 조건)
    
    1. 해(year)가 4의 배수이면서 100의 배수가 아니면 윤년.
    
    2. 400의 배수이면 윤년.
    
    위 두 조건 중 하나라도 맞으면 윤년이다.

---
+++ 1166_12059633(AC)0201.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+	int iYear;
+	string sMessage;
+	cin >> iYear;
+	while (true) {
+		if (iYear % 4 == 0 && iYear % 100 != 0) {
+			sMessage = "yes";
+			break;
+		}
+		else if (iYear % 400 == 0) {
+			sMessage = "yes";
+			break;
+		}
+		else {
+			sMessage = "no";
+			break;
+		}
+	}
+	cout << sMessage;
+
+	return 0;
+}
