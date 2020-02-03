371928080ab3b29191268ea0b94e07868aebe19a
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-03 20:05:27 +0900

    Correct1167
    bubble sorting
    세 개의 숫자가 주어질 때 두번째로 작은 수를출력해보자.
    
    예를 들어, 5 9 2 가 있다면 두번째로 작은 수는 5이다.
    
    입력
    세 개의 정수가 공백으로 구분되어 입력된다.
    
    출력
    세 개의 정수 중 두번째로 작은 숫자를 출력한다.

---
+++ 1167_12068070(AC)0203_bubble.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+
+	int iNumber[3], iIndex,jIndex,iTemp;
+	for (iIndex = 0; iIndex < 3; iIndex++) {
+		cin >> iNumber[iIndex];
+	}
+	
+	for (iIndex = 0; iIndex < 3; iIndex++) {
+
+		for (jIndex = 0; jIndex < 2; jIndex++) {
+			if (iNumber[jIndex] > iNumber[jIndex+1]) {
+				iTemp = iNumber[jIndex];
+				iNumber[jIndex] = iNumber[jIndex + 1];
+				iNumber[jIndex+1] = iTemp;
+			}
+		}
+	}
+	cout << iNumber[iIndex - 2];
+
+	return 0;
+}
