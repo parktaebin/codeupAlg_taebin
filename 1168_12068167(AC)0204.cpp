a5d808f214cbddef3c51724d618bebb7d017069a
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-04 20:21:23 +0900

    Create 1168_12068167(AC)0204.cpp
    주민등록번호는 생년월일과 성별정보, 지역정보로 이루어진다.
    
    여기서 생년월일과 성별정보만 입력으로 받겠다.
    
    성별 정보는 1이면 1900년대 출생 남자, 2이면 1900년대 출생 여자, 3이면 2000년대 출생 남자, 4이면 2000년대 출생 여자를 말한다.
    
    기준년도는 2012년도이다. 현재 나이를 출력하시오.
    
    예)
    
    790101 1        =====> 성별정보가 1이므로, 1979년생, 34살이다.
    
    080521 4        =====>  성별정보가 4이므로, 2008년생,  5살이다

---
+++ 1168_12068167(AC)0204.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+
+	int idNumber[2],iYear,iIndex,iAge;
+
+	for (iIndex = 0; iIndex < 2; iIndex++) {
+		cin >> idNumber[iIndex];
+	}
+	iYear = idNumber[0] / 10000;
+
+	if (idNumber[1] == 3 || idNumber[1] == 4) {
+		iAge = 12 - iYear + 1;
+	}
+	else {
+		iAge = 100 - iYear + 13;
+	}
+	cout << iAge;
+
+	return 0;
+}
