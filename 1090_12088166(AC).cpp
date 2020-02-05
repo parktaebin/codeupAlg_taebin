c6b25e382603323a7038dfb246511bcfaa2c6ebf
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-05 21:09:33 +0900

    Correct 1090
    10x10x10의 경우에 오버플로우가 나서, datatype을 int->long long으로 변경했다
    *************************************
    문제
    어떤 규칙에 따라 수를 순서대로 나열한 것을 수열이라고 한다.
    
    예를 들어
    2 6 18 54 162 486 ... 은
    2부터 시작해 이전에 만든 수에 3을 곱해 다음 수를 만든 수열이다.
    
    이러한 것을 수학에서는 앞뒤 수들의 비율이 같다고 하여
    등비(비율이 같다의 한문 말) 수열이라고 한다.
    
    등비 수열을 알게된 영일이는 갑자기 궁금해졌다.
    
    "그럼.... 13번째 나오는 수는 뭘까?"
    
    영일이는 프로그램을 만들어 더 큰 수도 자동으로 계산하고 싶어졌다.
    
    시작 값(a), 등비(r), 몇 번째인지를 나타내는 정수(n)가 입력될 때
    n번째 수를 출력하는 프로그램을 만들어보자.
    
    입력
    시작 값(a), 등비의 값(r), 몇 번째 인지를 나타내는 정수(n)가
    공백을 두고 입력된다.(모두 0 ~ 10)
    
    출력
    n번째 수를 출력한다.

---
+++ 1090_12088166(AC).cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+
+	long long iStartNumber,iRatio,iSequence,iIndex,iSum;
+	cin >> iStartNumber >> iRatio >> iSequence;
+
+	iSum =0;
+
+	for (iIndex = 0; iIndex < iSequence; iIndex++) {
+
+		iSum = iStartNumber+(iSum *iRatio);
+		iStartNumber = 0;
+	}
+
+	cout << iSum;
+
+	return 0;
+}
