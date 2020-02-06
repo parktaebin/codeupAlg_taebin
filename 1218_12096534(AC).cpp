7c5af6e118ef37d8fc576e81997d4f04a2690822
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-06 20:34:07 +0900

    Correct1218
    세 변수를 입력받아 외부의 함수에서 변수의 값들을 비교해서 값을 리턴함
    ****************************
    삼각형의 3변의 길이 a, b, c가 입력으로 주어진다.
    
    여기서 입력되는 변의 관계는 a ≤ b ≤ c 이다.
    
    그 삼각형이 무슨 삼각형인지 출력하시오.
    
    입력
    변의 길이 a, b, c가 입력된다. ( 정수)
    
    출력
    조건에 따라 삼각형을 출력한다.
    
    조건)
    
    세 변의 길이가 같은 경우 : 정삼각형
    
    두 변의 길이가 같은 경우 : 이등변삼각형
    
    a2 + b2 = c2일 경우(피타고라스 정리) : 직각삼각형
    
    위의 조건에 맞지 않는 일반 삼각형일 경우 : 삼각형
    
    삼각형이 아닐 경우 : 삼각형아님
    
    을 출력한다

---
+++ 1218_12096534(AC).cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int compare(int, int, int);
+
+int main() {
+
+	int iLengthA, iLengthB, iLengthC,iCompResult;
+	cin >> iLengthA >> iLengthB >> iLengthC;
+
+	
+		iCompResult=compare(iLengthA, iLengthB, iLengthC);
+
+		switch (iCompResult) {
+		
+		case -1:
+			cout << "삼각형아님";
+			break;
+		case 0:
+			cout << "삼각형";
+			break;
+		case 1:
+			cout << "정삼각형";
+			break;
+		case 2:
+			cout << "이등변삼각형";
+			break;
+		case 3:
+			cout << "직각삼각형";
+			break;
+		}
+	return 0;
+}
+int compare(int a, int b, int c) {
+
+	int iResult=0;
+
+	if (a + b <= c) {
+		iResult = -1;
+		}
+		else if (a == b && b==c) {
+			iResult = 1;
+		}
+		else if ((a == b && a != c)||(b == c && b != a) || (a == c && a != b)) {
+			iResult = 2;
+		}
+		else if (a * a + b * b == c * c) {
+			iResult = 3;
+		}
+
+	return iResult;
+}
