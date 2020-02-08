7ea4617b0cd120ddb647337b9595874320dde755
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-08 10:48:04 +0900

    correct 1261
    10개의 수가 입력된다.
    
    10개의 수 중 5의 배수를 하나만 출력한다.
    
    만약 5의 배수가 없다면 0을 출력한다.
    
    입력
    10개의 자연수가 입력된다
    
    출력
    10개의 수 중 5의 배수가 있으면 그 중 하나만 출력하고, 없다면 0을 출력한다.
    
    입력 예시
    1 2 3 4 5 6 7 8 9 10
    
    출력 예시
    5

---
+++ 1261_12106749(AC).cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int iNumber[10],iIndex,iPrintNumber;
+
+	for (iIndex = 0; iIndex < 10; iIndex++) {
+		cin >> iNumber[iIndex];
+	}
+	iPrintNumber = 0;
+
+	for (iIndex = 0; iIndex < 10; iIndex++) {
+
+		if (iNumber[iIndex] % 5 == 0) {
+			iPrintNumber = iNumber[iIndex];
+			break;
+		}
+	}
+	cout <<iPrintNumber;
+
+	return 0;
+}
