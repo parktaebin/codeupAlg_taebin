20c002af318af762c50ea6aafa81092c9ff9ecaf
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-08 12:43:07 +0900

    Create 1255_12107853(AC).cpp
    두수가 마이너스인 경수 반복횟수 고려해줌
    ************************
    소수 둘째 자리의 두 실수 a와 b가 입력으로 주어진다.
    
    a와 b사이의 수를 0.01간격으로 오름차순으로 출력하시오.
    
    예)
    
    5.67 5.73  ==> 5.67 5.68 5.69 5.70 5.71 5.72 5.73
    
    입력
    두 실수 a와 b가 입력된다. (a <= b)

---
+++ 1255_12107853(AC).cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	float fFirstNumber, fLastNumber, fIndex,fCount;
+
+	cin >> fFirstNumber >> fLastNumber;
+
+	if (fLastNumber<0&& fFirstNumber < 0) {
+		fCount = (fLastNumber - fFirstNumber + 0.01) * 100 -1;
+	}
+	else {
+		fCount = (fLastNumber - fFirstNumber + 0.01) * 100;
+	}
+	
+
+	for (fIndex =0; fIndex < fCount; fIndex++) {
+
+		cout << fixed;								//소숫점 자리 2자리로 출력고정
+		cout.precision(2);
+		cout << fFirstNumber<<' ';
+		fFirstNumber=fFirstNumber + 0.01;
+	}
+
+	return 0;
+}
