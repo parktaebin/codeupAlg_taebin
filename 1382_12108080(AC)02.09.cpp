b0f4b9d11c929963ce99d60a6cdca9efc084afdb
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-09 16:33:10 +0900

    correct 1382
    모 드라마의 한 장면에서 어떤 남자가 신의 속도로 코딩을 하면서,
    
    "1줄로 짤 코드를 누가 10줄로 만들어 놓았어요?"
    
    라고 말하는 장면이 프로그래머들 사이에서 주목을 받게 되었다.
    
    관련 동영상 보기(클릭)
    
    다시 보기로 분석한 결과 이 남성이 작성한 코드는 구구단 중 2~5단을 예쁘게 출력하는 클래스인 것으로 확인되었다.
    
    일명 GuguClass !!
    
    이 남자가 작성한 코드의 실행 결과는 다음과 같다.
    
    이 GuguClass와 동일한 결과를 나타내는 프로그램을 작성하시오.
    
    만약 출력문으로만 작성하거나, 코드가 길면 이 남자가 가만히 있을 것 같지 않다

---
+++ 1382_12108080(AC)02.09.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int iDan, iTimedNumber, iResult;
+
+	for (iTimedNumber = 1; iTimedNumber <= 9; iTimedNumber++) {
+
+		for (iDan = 2; iDan <= 5; iDan++) {
+			if (iDan * iTimedNumber < 10) {
+				cout << iDan << " x " << iTimedNumber << " = " <<' '<<iDan * iTimedNumber << '\t';
+			}
+			else {
+				cout << iDan << " x " << iTimedNumber << " = " << iDan * iTimedNumber << '\t';
+			}
+		}
+		cout << '\n';
+	}
+	return 0;
+}
