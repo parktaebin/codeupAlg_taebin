ea8098c4dca7bf16a9406eac72bfe243daf4571a
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-10 05:26:38 +0900

    correct 1359
    길이 n
    n
    이 입력되면 다음과 같은 숫자 피라미드를 출력한다.
    
    예)
    
    n
    n
    이 5이면
    
    1
    
    1 2
    
    1 2 3
    
    1 2 3 4
    
    1 2 3 4 5

---
+++ 1359_12116793(AC)02_10.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int iLine, iLineIndex, iIndex;
+	cin >> iLine;
+	for (iLineIndex = 0; iLineIndex < iLine; iLineIndex++) {
+
+		for (iIndex = 0; iIndex < iLineIndex+1; iIndex++) {
+
+			cout << iIndex+1 << ' ';
+
+		}
+		cout << endl;
+	}
+	return 0;
+}
