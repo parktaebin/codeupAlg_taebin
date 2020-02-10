8a611bf92ab36b4aba181788ecbd1866b9959d27
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-11 05:31:38 +0900

    correct 1361
    n이 입력되면 n층의 별 계단을 출력하시오.
    
    예) n= 5인 경우,
    
    **
     **
      **
       **
        **

---
+++ 1361_12116866(AC)02_11.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int iStar,iBlankIndex,iLine;
+	cin >> iStar;
+	for (iLine = 0; iLine < iStar; iLine++) {
+
+		for (iBlankIndex = 0; iBlankIndex < iLine; iBlankIndex++) {
+			cout << ' ';
+		}
+		cout << "**" << endl;
+	}
+	return 0;
+}
