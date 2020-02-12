7f0cfb63699b2cb1b7a32dfefc70d6230dfbbc87
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-12 20:43:52 +0900

    correct1416
    2진수 변환문제
    ***************
    어떤 10진수 n이 주어지면 2진수로 변환해서 출력하시오.
    
    예)
    
    10    ----->  1010
    
    0    ----->  0
    
    1    ----->  1
    
    2    ----->  10
    
    1024    ----->  1000000000

---
+++ 1416_12117139(AC)02_12.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int iSumIndex, iIndex = 0;
+	long long iNumber,iBinary, iSum[9999];
+	cin >> iNumber;
+	if (iNumber == 0) {
+		cout << 0;
+	}
+	else {
+		while (iNumber > 0) {
+			iBinary = iNumber % 2;
+			iSum[iIndex] = iBinary;
+			iNumber = iNumber / 2;
+			iIndex = iIndex + 1;
+
+		}
+		for (iSumIndex = iIndex; iSumIndex > 0; iSumIndex--) {
+			cout << iSum[iSumIndex - 1];
+		}
+	}
+	
+	return 0;
+}
