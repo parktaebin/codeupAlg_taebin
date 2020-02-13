ca50fd04ddc99e4760090fbca3168f178bb39a29
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-13 20:41:08 +0900

    Correct 1411
    우리는 1부터 N까지의 숫자가 차례대로 적힌 N장의 카드 묶음을 가지고 있다.
    
    그런 데 이 카드 묶음을 옮기는 중 실수로 땅에 떨어뜨려 그 중 한 장을 잃어버렸다.
    
    여러 분은 땅에 떨어진 카드 묶음을 읽어서 빠진 하나의 카드 번호를 찾아 출력해야 한다.
    
    입력
    첫 줄에는 한 장을 잃어버리기 전 카드의 전체 장수 N이 주어져 있다. 단 . 3 <= N <= 50 이다.
    
    이어지는 N-1개의 각 줄에는 한 장이 빠진 카드 묶음의 카드 숫자가 하나씩 순서 없이 나열되어 있다.
    
    출력
    여러분은 주 어진 카드 묶음에서 빠진 하나의 카드를 찾아서 그 번호를 출력해야 한다.
    
    입력 예시
    10
    3
    4
    1
    10
    2
    6
    7
    5
    9
    
    출력 예시
    8

---
+++ 1411_12117310(AC)02_13(2013대회준비문제).cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int iCardCount,iIndex,iNumber;
+	cin >> iCardCount;
+	int* p = new int[iCardCount];
+
+	for (iIndex = 0; iIndex < iCardCount; iIndex++) {
+		p[iIndex] = iIndex + 1;							//배열의 인덱스에 순차대로 1,2,3...iCount-1의 수저장
+	}																		
+
+	for (iIndex = 0; iIndex < iCardCount - 1; iIndex++) {
+		cin >> iNumber;									//숫자입력
+		p[iNumber - 1] = 0;								//해당 숫자를 이용해 배열의 인덱스-1번째 저장된 수를 0으로 변경
+														//ex)1입력시 0번째에 저장된 숫자 1을 0으로 변경
+	}
+	for (iIndex = 0; iIndex < iCardCount; iIndex++) {
+		if (p[iIndex] != 0) {
+			cout << p[iIndex];
+		}
+	}
+	delete []p;
+	return 0;
+}
