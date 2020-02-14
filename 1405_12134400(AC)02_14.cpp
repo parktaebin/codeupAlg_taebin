c9ce622ae1fc558fb70fa020166175991876d0ea
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-14 21:08:26 +0900

    Correct 1405
    n개의 숫자가 입력되면,
    
    n개의 숫자를 왼쪽으로 하나씩 돌려서 출력하시오.
    
    예) 1 2 3 4 5가 입력된 경우,
    
    1 2 3 4 5
    
    2 3 4 5 1
    
    3 4 5 1 2
    
    4 5 1 2 3
    
    5 1 2 3 4
    
    입력
    첫째 줄에 숫자의 개수 n이 입력된다.( 1 <= n <= 1,000)
    
    둘째 줄에 n개의 정수 k가 공백으로 구분되어 입력된다.(1 <= k <= 1,000)
    
    출력
    숫자를 로테이션한 결과를 출력한다.(단, 왼쪽으로만 돌린다.)
    
    입력 예시
    5
    1 2 3 4 5
    
    출력 예시
    1 2 3 4 5
    2 3 4 5 1
    3 4 5 1 2
    4 5 1 2 3
    5 1 2 3 4

---
+++ 1405_12134400(AC)02_14.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+	string sRotatMessage,iFirstStr,iLastStr;
+	int iArraySize,iIndex,iBlankpos;
+	cin >> iArraySize;
+
+	int* p = new int[iArraySize];									//메모리의 효율적인 사용을 위한 동적할당
+
+	for (iIndex = 0; iIndex < iArraySize; iIndex++) {
+		cin >> p[iIndex];
+	}
+	for (iIndex = 0; iIndex < iArraySize; iIndex++) {
+		sRotatMessage = sRotatMessage + to_string(p[iIndex])+' ';  //int배열안에 있는 수들을 문자열로 변환해서 저장
+	}
+	for (iIndex = 0; iIndex < iArraySize; iIndex++) {
+		cout << sRotatMessage<<endl;
+		iBlankpos=sRotatMessage.find(' ');							//공백의 인덱스를 찾기->수의 자릿수가 다를수 있음	
+		iFirstStr = sRotatMessage.substr(0, iBlankpos+1);			//0번째부터 공백까지의 n+1(공백포함)개의 문자열분리
+		iLastStr = sRotatMessage.substr(iBlankpos+1);				//나머지 문자들을 (2번째부터) 분리 
+		sRotatMessage = iLastStr + iFirstStr;						//두 문자열을 연결해서 새로운 문자열 만듦
+
+	}
+
+	delete []p;
+	return 0;
+}
