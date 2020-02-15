b19426a5e29e37fc8ed27e0e1ff6f772fb658dca
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-15 22:14:20 +0900

    Correct 1127
    철수는 자신의 성적을 꼼꼼하게 관리한다.
    
    매번 시험을 치고 나면 자신의 성적을 기록해 놓고 기말고사가 끝나면 중간고사, 기말고사, 수행평가의 점수를 반영비율에 맞게 합산한다.
    
    최근에 프로그래밍 수행평가를 치고 프로그래밍 과목의 점수를 계산하게 되었다.
    
    반영비율을 알고 있고 각각의 점수를 알고 있다면 이 과목의 점수는 다음과 같이 계산할 수 있다.
    
    중간 고사	기말 고사	수행 평가
    반영 비율	30%	30%	40%
    받은 점수	86	90	85
    0.3 × 86 + 0.3 × 90 + 0.4 × 85 = 86.8
    
    이런 방법으로 전 과목의 점수를 계산해야 한다.
    
    일일이 계산하기 어려우므로 철수는 이 과정을 프로그래밍하기로 하였다.
    
    반영비율과 점수를 알고 있을 때 각 과목의 점수를 계산하는 프로그램을 작성하시오.
    
    입력
    ① 반영 비율과 받은 점수를 입력한다.
    
    ② 입력 순서는 중간고사 반영비율, 중간고사 점수, 기말고사 반영비율, 기말고사 점수, 수행평가 반영비율, 수행평가 점수 순이다.
    
    ③ 반영비율은 실수형, 각 점수는 정수형으로 입력한다.
    
    출력
     결과를 실수로 출력하되 소수 첫째 자리까지 출력한다.

---
+++ 1127_12163431(AC).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	double dRatio[3], dFinalScore;
+	int iScore[3], iIndex;
+
+	dFinalScore = 0;
+
+	for (iIndex = 0; iIndex < 3; iIndex++) {
+		cin >> dRatio[iIndex] >> iScore[iIndex];
+		dFinalScore = dFinalScore + dRatio[iIndex] * iScore[iIndex];
+	}
+	cout << fixed;
+	cout.precision(1);
+	cout << dFinalScore;
+
+	return 0;
+}
+
