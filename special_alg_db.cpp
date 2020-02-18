bf9954a708dca3b6aee59fbb6ac94b42fadd96fa
parktaebin <58967138+parktaebin@users.noreply.github.com>
2020-02-18 20:18:44 +0900

    문제 번호 A: DB등록 자동화
    병찬이는 창원과학고등학교 전자 활동승인서 사이트를 관리한다.
    
    전자 활동승인서 사이트에 친구들이 가입하려면 학반번호, 이름 그리고 인증 코드를 미리 데이터베이스에 추가해야 한다.
    
    이 작업에 귀찮음을 느낀 병찬이는 평소 자주 하던 코딩을 통해 이것을 자동화하려고 한다.
    
    학반번호, 이름 그리고 인증 코드를 읽고 DB 양식에 맞게
    
    "학반번호","이름","인증 코드","가입 여부","비밀번호","더미 데이터 1","더미 데이터 2"를 출력해야 한다.
    
    입력
    1번째 줄에 학생 수 n(n<=1,000)과 인증 코드가 주어지고,
    
    2번째 줄부터 n+1줄까지 학반번호, 이름이 ","로 구분되어 주어진다.
    
    단 인증 코드는 6바이트이고 이름은 10바이트 이내이다. 학번은 4자리수 숫자로 구성된다.
    
    출력
    "학반번호","이름","인증 코드","가입 여부","비밀번호","더미 데이터 1","더미 데이터 2" 이런 식으로 입력한 순서대로 출력해야 한다. (따옴표도 출력해야 한다. )
    
    입력 예시
    3 cs2020
    1101,김철수
    1102,이영희
    1103,홍길동
    
    출력 예시
    "1101","김철수","cs2020","0","","0","0"
    "1102","이영희","cs2020","0","","0","0"
    "1103","홍길동","cs2020","0","","0","0"

---
+++ special_alg_db.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+
+	string identCode;
+	int iStudentNumber, iIndex;
+	cin >> iStudentNumber >> identCode;
+
+	string* pClass = new string[iStudentNumber];
+	string* pName = new string[iStudentNumber];
+
+	for (iIndex = 0; iIndex < iStudentNumber; iIndex++) {
+
+		getline(cin, pClass[iIndex], ',');
+		cin >> pName[iIndex];
+	}
+	for (iIndex = 0; iIndex < iStudentNumber; iIndex++) {
+		cout<< "\"" << pClass[iIndex] <<"\",\""<< pName[iIndex] << "\",\"" <<
+			identCode << "\",\"" << "0" << "\",\"" << " " << "\",\"" << "0" << "\",\"" << "0" << "\"" << endl;
+	}
+
+	delete[]pName;
+	delete[]pClass;
+	return 0;
+}
